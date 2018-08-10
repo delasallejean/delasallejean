f3589588cc6259d15ea73c8e7514ef52dfa28eb9
jean delasalle <jeandelasalle@outlook.fr>
2018-08-11 01:25:42 +0200

    plagia

--- jsx/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Document from './document.jsx';
-
-ReactDOM.render(
-    <Document documentId="2409" title="document title updated"/>,
-    document.querySelector('#document')
-);
